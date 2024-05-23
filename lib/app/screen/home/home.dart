3169d3b58ba1a2e453d7f5254a83689386ad4f67
Taha_Nadeem <tahanadeem.gk@gmail.com>
2024-05-23 08:13:59 -0700

    New Updated Home Screen

--- lib/app/screen/home/home.dart
+++ lib/app/screen/home/home.dart
@@ -4,12 +4,17 @@ import 'package:labquest/app/core/extensions.dart';
 import 'package:labquest/app/screen/home/widgets/addCard.dart';
 import 'package:labquest/app/screen/home/widgets/taskCard.dart';
 import 'package:labquest/controller/homecontroller.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class Home extends GetView<Homecontroller> {
   Home({Key? key}) : super(key: key);
   var control=Get.find<Homecontroller>();
   @override
   Widget build(BuildContext context) {
+    var createdTasks = control.getTotalTask();
+    var completedTasks = control.getTotalDoneTask();
+    var livetasks = createdTasks-completedTasks;
+    var precent=(completedTasks/createdTasks *100).toStringAsFixed(0);
     return Scaffold(
       body: SafeArea(child: 
       ListView(
@@ -25,6 +30,49 @@ class Home extends GetView<Homecontroller> {
                 child: Text('My List',style: Theme.of(context).textTheme.headline4,),
               ),
               ],),
+
+          Padding(
+            padding:  EdgeInsets.symmetric(vertical: 4.0.wp,horizontal: 4.0.wp),
+            child: const Divider(thickness: 2,),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 3.0.wp,horizontal: 5.0.wp),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                status(Colors.red, livetasks, 'Live Tasks', context),
+                status(Colors.green, completedTasks, 'Completed', context),
+                status(Colors.blue, createdTasks, 'Created', context)
+              ],
+            ),
+          ),
+          SizedBox(height:15.0.wp),
+          UnconstrainedBox(
+            child: SizedBox(
+              width: 70.0.wp,
+              height: 70.0.wp,
+              child: CircularStepProgressIndicator(
+                totalSteps: createdTasks==0?1:createdTasks,
+                currentStep: completedTasks,
+                stepSize: 20,
+                selectedColor: Colors.deepPurple,
+                unselectedColor: Colors.grey[200],
+                padding: 0,
+                width:150,
+                height: 150,
+                selectedStepSize: 22,
+                roundedCap: (_,__)=>true,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('${createdTasks==0?0:precent} %',style: Theme.of(context).textTheme.headline4,),
+                    SizedBox(height: 1.0.wp,),
+                    Text("Efficiency",style: Theme.of(context).textTheme.headline2,)
+                  ],
+                ),
+              ),
+            ),
+          ),
           Obx((){
             return GridView.count(
               crossAxisCount: 2,
@@ -47,6 +95,34 @@ class Home extends GetView<Homecontroller> {
       )),
       
       
+    );
+  }
+
+  Widget status(Color color,int number,String title,BuildContext context){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 3.0.wp,
+          width: 3.0.wp,
+          decoration: BoxDecoration(
+              color: color.withOpacity(0.4),
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 0.5.wp,
+                color: color,
+              )
+          ),
+        ),SizedBox(width: 3.0.wp,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("$number",style: Theme.of(context).textTheme.headline6,),
+            Text(title,style: Theme.of(context).textTheme.headline2,)
+
+          ],
+        )
+      ],
     );
   }
 }
